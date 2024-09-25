fab6e75bee3345fab484edec55f503e020781e5c
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-09-25 14:54:37 +0530

    Create bitmap.c

---
+++ bitmap.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+
+
+void main()
+{
+int a[100][100];
+
+  
+  int i, j, k, l = 99;
+  int gd=DETECT,gm;
+  
+  initgraph(&gd,&gm,"c:\\tc\\bgi");
+  
+  for (k = 0; k < 100; k++)
+  {
+      for (l = 0; l < 100; l++)
+      {
+          if (k == l)
+          {
+              a[k][l] = 1;
+          }
+          else
+          {
+              a[k][l] = 0;
+          }
+      }
+  }
+  
+  for (k = 0; k < 100; k++)
+  {
+    a[k][l] = 1;
+    l--;
+  }
+  
+  
+  for(i=0;i<100;i++)
+  {
+    for(j=0;j<100;j++)
+    {
+      putpixel(200+i,200+j,10*a[i][j]);
+    }
+  }
+  getch();
+  closegraph();
+}
+  
